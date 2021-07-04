1761f9c4976ab7f086a6e6cc2fed99f3884421a2
vinayr <vinay.rv@gmail.com>
2021-07-04 21:42:46 +0530

    update class name since imdb updated its UI

--- src/showrate.js
+++ src/showrate.js
@@ -1,13 +1,11 @@
-// prettier-ignore
-
-$(document).ready(function() {
+$(document).ready(function () {
   const baseUrl = "https://www.imdb.com";
-  const blacklist = [ "TV", "Short", "Video", "Documentary" ];
+  const blacklist = ["TV", "Short", "Video", "Documentary"];
 
   // For search window
   const target = document.querySelector(".react-autosuggest__container");
-  const observer = new window.WebKitMutationObserver(function(mutations) {
-    $("a[data-testid='search-result--const']").each(function() {
+  const observer = new window.WebKitMutationObserver(function (mutations) {
+    $("a[data-testid='search-result--const']").each(function () {
       const link = $(this).attr("href");
       // console.log("link", link);
       const titlePath = link.split("?")[0];
@@ -22,19 +20,19 @@ $(document).ready(function() {
   // For actor pages
   if ($(".filmo-row")[0]) {
     let time = 100;
-    $(".filmo-row").each(function() {
+    $(".filmo-row").each(function () {
       const link = $(this).find("a:first").attr("href");
       const titlePath = link.split("?")[0];
       const movieUrl = baseUrl + titlePath;
-      // showRatingsInPage($(this), movieUrl);
+      // throttle
       setTimeout(() => showRatingsInPage($(this), movieUrl), time);
       time += 100;
     });
   }
 
   function showRatingsInSearch(obj, url) {
-    $.get(url, function(data) {
-      let rating = $(data).find("span[itemprop='ratingValue']").first().text();
+    $.get(url, function (data) {
+      let rating = $(data).find("span[class^=AggregateRatingButton__RatingScore]").first().text();
       if (!rating) rating = "0.0";
       rating = parseFloat(rating).toFixed(1);
       obj.append(`<span>${rating}</span>`);
@@ -42,14 +40,14 @@ $(document).ready(function() {
   }
 
   function showRatingsInPage(obj, url) {
-    $.get(url, function(data) {
-      let rating = $(data).find("span[itemprop='ratingValue']").first().text();
+    $.get(url, function (data) {
+      let rating = $(data).find("span[class^=AggregateRatingButton__RatingScore]").first().text();
       if (!rating) rating = "0.0";
       rating = parseFloat(rating).toFixed(1);
 
       let color = "blue";
       if (rating >= 7.5) {
-        //check if not movie
+        // check if not movie
         const content = obj.clone().children().remove().end().text();
         if (!blacklist.some(b => content.includes(b))) color = "red";
       }
